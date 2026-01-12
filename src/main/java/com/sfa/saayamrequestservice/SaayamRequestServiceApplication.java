247ab68852ad940c753a95bda10a56e974b1fbea
vanitha-92 <vanithas.kat@gmail.com>
2026-01-11 18:45:52 -0600

    updated docker files

---
+++ src/main/java/com/sfa/saayamrequestservice/SaayamRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sfa.saayamrequestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaayamRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaayamRequestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfa/saayamrequestservice/SaayamRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfa.saayamrequestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaayamRequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
